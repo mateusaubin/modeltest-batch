6e3956ed5a842af6c53370b694cb5cd53af2dce3
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-24 12:55:56 -0300

    small logger sample

---
+++ logme.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+import logging
+import time
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)-15s | %(levelname)-8s | %(message)s'
+)
+logger = logging.getLogger()
+
+print('helo')
+
+time.sleep(2)
+logger.debug('buga buga buga')
+
+time.sleep(2)
+logger.info('tamo no docker?')
+
+time.sleep(2)
+logger.warning('avizao')
+
+time.sleep(2)
+logger.error('errado')
+
+time.sleep(2)
+logger.critical('ja era')
+
+time.sleep(2)
+print('goodbye')
