66bfeb2a777091045e8baea1297e50c2b18c1bd2
mateusaubin <mateusaubin@users.noreply.github.com>
2018-09-29 17:27:33 -0300

    build sample docker image for phyml

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,21 @@
 
 FROM python:3.6
 
+COPY requirements.txt /
+RUN pip install -r /requirements.txt
+
 # Define Python required libraries in requirements.txt
 # Copy requirements file into the Docker image
 #COPY requirements.txt /tmp/requirements.txt
 
+WORKDIR /app
+
+COPY lib/phyml lib/phyml
+COPY lib/small.aP6.phy tmp/small.aP6.phy
+
 COPY logme.py logme.py
 
+
 #CMD [ "python", "-u", "vai.py" ] unbuffered
-CMD [ "./logme.py" ]
+ENTRYPOINT [ "python3", "logme.py" ]
+CMD [ "Ref::phyfilepath" ]
--- logme.py
+++ logme.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 import logging
 import time
+import os
+import subprocess
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)-15s | %(levelname)-8s | %(message)s'
+    format='%(levelname)-8s | %(message)s'
 )
 logger = logging.getLogger()
 
-print('helo')
+phyfilepath = sys.argv[1]
 
 time.sleep(2)
-logger.debug('buga buga buga')
+logger.debug('buga {} buga'.format(phyfilepath))
 
 time.sleep(2)
 logger.info('tamo no docker?')
@@ -22,10 +25,45 @@ time.sleep(2)
 logger.warning('avizao')
 
 time.sleep(2)
-logger.error('errado')
+logger.error('errado {} errado'.format(phyfilepath))
 
 time.sleep(2)
 logger.critical('ja era')
 
-time.sleep(2)
-print('goodbye')
+cwd = os.getcwd()
+logger.critical(cwd)
+
+
+cmdline_args = [os.path.join(cwd, 'lib', 'phyml'), ]
+cmdline_args.extend(['-i', 'tmp/small.aP6.phy'])
+cmdline_args.extend("-d nt -n 1 -b 0 --run_id GTR+I+G -m 012345 -f m -v e -c 4 -a e --no_memory_check --r_seed 12345 -o tlr -s BEST".split())
+
+trace_file = os.path.join(
+            cwd,
+            'tmp/',
+            "trace_{}.log".format('jmodel_runid')
+        )
+
+with open(trace_file, "w") as file:
+    result = subprocess.run(cmdline_args,
+                            stdout=file,
+                            stderr=subprocess.STDOUT)
+
+logging.warn("PhyML.ReturnCode={}".format(result.returncode))
+resultfiles = [x for x in os.listdir(os.path.join(cwd, 'tmp/')) if x != "_input"]
+# debug por enquanto
+logging.info(resultfiles)
+
+# bail out if phyml error'd
+# TODO: assert a existência dos 3 arquivos [ {filenamewithext}_phyml_stats_{run_id}, {filenamewithext}_phyml_tree_{run_id}, trace.log ]
+processData = subprocess.run(
+    ["cat", trace_file], 
+    stdout=subprocess.PIPE
+)
+logging.error(processData.stdout.decode('UTF-8'))
+
+if result.returncode != 0:    
+
+    raise subprocess.SubprocessError("Error calling PhyML")
+
+logger.critical('EH TETRAAAAAAA!!!!!!')
